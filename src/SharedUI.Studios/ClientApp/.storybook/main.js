0d77bdd710db298dc89c0e2f7db31b497bef1e12
Vishak Bharadwaj S <v-vishakb@microsoft.com>
2022-11-07 18:15:27 +0000

    Merged PR 7062030: Integrating accessibility add-on
    Integrating accessibility add-on![Accessibility add-on (2).png](https://msazure.visualstudio.com/a531312f-ba7b-4573-ab4f-d468b740a319/_apis/git/repositories/b7a9a7cf-d195-4fc7-8936-cf97cb9317f1/pullRequests/7062030/attachments/Accessibility%20add-on%20%282%29.png) ![Passes.png](https://msazure.visualstudio.com/a531312f-ba7b-4573-ab4f-d468b740a319/_apis/git/repositories/b7a9a7cf-d195-4fc7-8936-cf97cb9317f1/pullRequests/7062030/attachments/Passes.png)

--- src/SharedUI.Studios/ClientApp/.storybook/main.js
+++ src/SharedUI.Studios/ClientApp/.storybook/main.js
@@ -10,7 +10,8 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@react-theming/storybook-addon"
+    "@react-theming/storybook-addon",
+    "@storybook/addon-a11y"
   ],
   framework: "@storybook/react",
   core: {
