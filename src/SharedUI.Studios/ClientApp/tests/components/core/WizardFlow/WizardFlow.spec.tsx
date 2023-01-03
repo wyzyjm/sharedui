8c8bf3c40cf5fce4af790a4610ef9c7ee4427b95
Vishak Bharadwaj S <v-vishakb@microsoft.com>
2023-01-03 23:32:25 +0000

    Merged PR 7396629: Added tests for Wizard Flow
    # Description
    
    Describe what your change does here.
    
    # Confirm you have done the below:
    
    - [ ] Have you uploaded a screenshot in the description field (if UI has been updated)?
    - [ ] Have you exported the component and type in the index.tsx file?
    - [ ] Have you added tests for the component? Refer to any existing test file for reference.
    - [ ] Have you linked a work item in the PR? If item doesnt exit, create a new work item and link it.
    - [ ] Have you done a self-review? If yes, make sure you sign off on your own PR.
    - [ ] Have you made sure all the accessibility issues have been fixed for the components in the PR?
    - [ ] If adding a new iteration, make sure all the comments are fixed and marked as "resolved" or "wont fix"
    
    ## Reminder
    
    - [ ] If your build fails for your PR, you are needed to fix it.
    
    Updated WizardFlow tests for properties
    
    Related work items: #16624451

--- src/SharedUI.Studios/ClientApp/tests/components/core/WizardFlow/WizardFlow.spec.tsx
+++ src/SharedUI.Studios/ClientApp/tests/components/core/WizardFlow/WizardFlow.spec.tsx
@@ -4,35 +4,88 @@ import { StorybookTestPOM } from '../../../StoryBook.spec';
 let storybookPOM: StorybookTestPOM;
 
 const dialogMainClass = ".ms-Dialog-main";
-const wizardHeaderClass = ".ms-Dialog-title";
+const wizardTitleClass = ".ms-Dialog-title";
 
 test.beforeEach(async ({ page }) => {
   storybookPOM = new StorybookTestPOM(page);
   await storybookPOM.goto();
 
-  const pageUrl = storybookPOM.getBaseUrl() + "?path=/story/wizardflow--wizard-flow";
+  const pageUrl = storybookPOM.getBaseUrl() + "?path=/story/wizard--wizard";
   await page.goto(pageUrl);
 });
 
 
 test.describe('WizardFlow tests', () => {
   test('should render wizardFlow as per design', async ({ page }) => {
-  //   const dialogMainElement = await storybookPOM.getElementFromPreview(dialogMainClass);
+    const dialogMainElement = await storybookPOM.getElementFromPreview(dialogMainClass);
+    const wizardTitleElement = await storybookPOM.getElementFromPreview(wizardTitleClass);
 
-  //   // Check background color
-  //   const backgroundColor = await storybookPOM.getComputedStyle(dialogMainElement, "background-color");
-  //   await expect(backgroundColor).toBe("rgb(255, 255, 255)");
+    // Check background color
+    const backgroundColor = await storybookPOM.getComputedStyle(dialogMainElement, "background-color");
+    await expect(backgroundColor).toBe("rgb(255, 255, 255)");
 
-  //   // Check width
-  //   const width = await storybookPOM.getComputedStyle(dialogMainElement, "width");
-  //   await expect(width).toBe("800px");
+    // Check text color
+    const color = await storybookPOM.getComputedStyle(dialogMainElement, "color");
+    await expect(color).toBe("rgb(50, 49, 48)");
+
+    // Check font weight
+    const fontSize = await storybookPOM.getComputedStyle(dialogMainElement, "font-size");
+    await expect(fontSize).toBe("14px");
+
+    // Check font weight
+    const fontWeight = await storybookPOM.getComputedStyle(dialogMainElement, "font-weight");
+    await expect(fontWeight).toBe("400");
+
+    // Check width
+    const width = await storybookPOM.getComputedStyle(dialogMainElement, "width");
+    await expect(width).toBe("800px");
+
+    // Check height
+    const height = await storybookPOM.getComputedStyle(dialogMainElement, "height");
+    await expect(height).toBe("350px");
+
+    // Check title background color
+    const titleBackgroundColor = await storybookPOM.getComputedStyle(wizardTitleElement, "background-color");
+    await expect(titleBackgroundColor).toBe("rgba(0, 0, 0, 0)");
+
+    // Check text color
+    const titleColor = await storybookPOM.getComputedStyle(wizardTitleElement, "color");
+    await expect(titleColor).toBe("rgb(50, 49, 48)");
+
+    // Check font weight
+    const titleFontSize = await storybookPOM.getComputedStyle(wizardTitleElement, "font-size");
+    await expect(titleFontSize).toBe("20px");
+
+    // Check font weight
+    const titleFontWeight = await storybookPOM.getComputedStyle(wizardTitleElement, "font-weight");
+    await expect(titleFontWeight).toBe("600");
+
+    // Check width
+    const titleWidth = await storybookPOM.getComputedStyle(wizardTitleElement, "width");
+    await expect(titleWidth).toBe("783px");
+
+    // Check height
+    const titleHeight = await storybookPOM.getComputedStyle(wizardTitleElement, "height");
+    await expect(titleHeight).toBe("60px");
   });
 
-  test('to verify no accessibility violations are found', async ({ page }) => { 
-  //   // Check the Voilations tab under Accessibility
-  //   await page.getByRole('tab', { name: 'Accessibility' }).click();
-  //   await page.locator('button', { hasText: 'Violations' }).click();
-  //   await expect(page.locator('.sto-1551xjo')).toHaveText('0 Violations');
-  //   await expect(page.locator('.sto-snh8f7')).toContainText('No accessibility violations found.');
+  test('the wizard flow should update on properties updates', async ({ page }) => {
+    // Check the height prop
+    await page.locator('textarea[name="height"]').fill("10px");
+    const dialogMainElement = await storybookPOM.getElementFromPreview(dialogMainClass);
+    const height = await storybookPOM.getComputedStyle(dialogMainElement, "height");
+    await expect(height).toBe("244px");
+
+    // Check the dialog hidden
+    await page.locator('input[name="hidden"]').click();
+    await expect(await page.locator(dialogMainClass).count()).toEqual(0);
+  });
+
+  test('to verify no accessibility violations are found', async ({ page }) => {
+    // Check the Voilations tab under Accessibility
+    await page.getByRole('tab', { name: 'Accessibility' }).click();
+    await page.locator('button', { hasText: 'Violations' }).click();
+    await expect(page.locator('.sto-1551xjo')).toHaveText('0 Violations');
+    await expect(page.locator('.sto-snh8f7')).toContainText('No accessibility violations found.');
   });
 });
