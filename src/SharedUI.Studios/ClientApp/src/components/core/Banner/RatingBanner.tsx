c6a94ebb4f780de7a0cb290b8f7396df919c9028
Mukesh Agarwal <mukeshag@microsoft.com>
2023-01-19 08:09:26 +0000

    Merged PR 7487479: Moved scss to styled comps for ratings banner
    # Description
    
    Describe what your change does here.
    
    # Confirm you have done the below:
    
    - [ ] Have you uploaded a screenshot in the description field (if UI has been updated)?
    - [ ] Have you exported the component and type in the index.tsx file?
    - [ ] Have you added tests for the component? Refer to any existing test file for reference.
    - [ ] Have you linked a work item in the PR? If item doesnt exit, create a new work item and link it.
    - [ ] Have you done a self-review? If yes, make sure you sign off on your own PR.
    - [ ] Have you made sure all the accessibility issues have been fixed for the components in the PR?
    - [ ] If adding a new iteration, make sure all the comments are fixed and marked as "resolved" or "wont fix"
    
    ## Reminder
    
    - [ ] If your build fails for your PR, you are needed to fix it.
    
    Moved scss to styled comps for ratings banner
    
    Related work items: #16835537

--- src/SharedUI.Studios/ClientApp/src/components/core/Banner/RatingBanner.tsx
+++ src/SharedUI.Studios/ClientApp/src/components/core/Banner/RatingBanner.tsx
@@ -2,13 +2,12 @@
 import React, { useState } from "react";
 import { FontIcon, Text, DefaultButton, MessageBar, MessageBarType, ChoiceGroup, IChoiceGroupOption } from "@fluentui/react";
 import { initializeIcons } from '@fluentui/react/lib/Icons';
-import "./RatingBanner.scss";
-import "../core.scss";
 import { HaTSArea as CS_Hats, HatsProps } from '../Hats/Hats';
 import { INTL } from "../../../util/intlUtil";
 import { BannerLocalizationFormatMessages } from "../../../clientResources";
 initializeIcons(undefined, { disableWarnings: true });
 import { initializeComponent, useLocalization, withLocalization } from "../../../services/localization";
+import styled from "styled-components";
 
 export interface RatingBannerProps {
     onClose: Function;
@@ -16,6 +15,209 @@ export interface RatingBannerProps {
     hatsProp?: HatsProps;  // if hatsProp is passed, the hats panel would be shown on click of the banner.
     onClick: Function;
 };
+
+const StyledMessageBar = styled(MessageBar)`
+    position: absolute;
+    left: 0px;
+    right: 0px;
+    top: 40px;
+
+    @media only screen and (max-width: 1116px) {
+        height: 69px;
+        top: 40px;
+    }
+
+    @media only screen and (max-width: 904px) {
+        height: 79px;
+        top: 40px;
+    }
+
+    @media only screen and (max-width: 680px) {
+        height: 144px;
+        right: -3px;
+        top: 40px;
+    }
+
+    @media only screen and (max-width: 480px) {
+        position: absolute;
+        height: 150px;
+        top: 40px;
+    }
+
+    @media only screen and (max-width: 390px) {
+        height: 154px;
+        top: 40px;
+    }
+
+    @media only screen and (max-width: 320px) {
+        height: 420px;
+        top: 40px;
+    }
+`;
+
+const StyledRatingBannerDiv = styled.div`
+    .rating-number-label:last-child {
+        .ms-Label {
+            font-weight: 400;
+            padding-left: 2px;
+        }
+    }
+
+    .rating-number-label:not(:last-child) {
+        .ms-Label {
+          font-weight: 400;
+          padding-left: 5px;
+
+          @media only screen and (max-width: 390px) {
+            margin-left: 2px;
+            margin-right: 5px;
+          }
+        }
+    }
+
+    .studio-banner-left {
+        height: 100%;
+        display: flex;
+        overflow: hidden;
+        width: 100%;
+
+        @media only screen and (max-width: 680px) {
+            flex-direction: column;
+        }
+
+        @media only screen and (max-width: 390px) {
+            flex-direction: column;
+            margin-left: -30px;
+            width: calc(100% + 15px);
+        }
+    
+        .studio-banner-content {
+          margin: 0 8px;
+          cursor: 'pointer';
+          font-size: 12px;
+          vertical-align: top;
+
+          @media only screen and (max-width: 1116px) {
+            width: 45%;
+          }
+
+          @media only screen and (max-width: 904px) {
+            width: 25%;
+          }
+
+          @media only screen and (max-width: 680px) {
+            width: calc(100% - 32px);
+          }
+
+          @media only screen and (max-width: 390px) {
+            width: calc(100% - 32px);
+            margin-left: 28px;
+          }
+        }
+      }
+    
+      .studio-ratings {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        
+        @media only screen and (max-width: 680px) {
+            margin-top: 20px;
+        }
+    
+        .negative {
+          margin-right: 10px;
+          margin-left: 20px;
+
+          @media only screen and (max-width: 1116px) {
+            margin-right: 10px;
+            margin-left: 0px;
+          }
+
+          @media only screen and (max-width: 904px) {
+            margin-right: 10px;
+            margin-left: 0px;
+          }
+
+          @media only screen and (max-width: 680px) {
+            margin-left: 8px;
+          }
+
+          @media only screen and (max-width: 390px) {
+            margin-left: 4px;
+            margin-top: -5px;
+            align-items: left;
+          }
+        }
+    
+        .positive {
+          margin-left: 12px;
+
+          @media only screen and (max-width: 680px) {
+            margin-left: -90px;
+          }
+
+          @media only screen and (max-width: 390px) {
+            margin-right: 0px;
+            margin-top: -16px;
+          }
+
+          @media only screen and (max-width: 320px) {
+            margin-left: 0px;
+            margin-top: -4px;
+          }
+        }
+    
+        .ratings {
+          margin-top: -5px;
+
+          @media only screen and (max-width: 680px) {
+            margin-left: -60px;
+            margin-top: 20px;
+          }
+
+          @media only screen and (max-width: 390px) {
+            margin-left: 0px;
+            margin-top: 0px;
+          }
+        }
+      }
+    
+      #studio-rating-banner-close-button {
+        position: absolute;
+        width: 32px;
+        height: 32px;
+        right: 0px;
+        top: 0px;
+        font-family: 'Fabric MDL2 Assets';
+        font-style: normal;
+        margin-right: -5px;
+        font-weight: 400;
+        font-size: 10px;
+        line-height: 100%;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        border: none;
+        cursor: pointer;
+      }
+`;
+
+const StyledChoiceGroup = styled(ChoiceGroup)`
+    .ms-ChoiceField {
+        @media only screen and (max-width: 390px) {
+            display: inline-block;
+            font-family: 'Segoe UI';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 14px;
+            line-height: 20px;
+            text-align: center;
+            margin-right: .7px;
+        }
+    }
+`;
+
 
 const RatingBannerInternal = (props: RatingBannerProps) => {
 
@@ -40,8 +242,8 @@ const RatingBannerInternal = (props: RatingBannerProps) => {
         <div>
             {isBannerVisible
                 && (
-                    <div className="studio-rating-banner">
-                        <MessageBar
+                    <StyledRatingBannerDiv className="studio-rating-banner">
+                        <StyledMessageBar
                             className="message-bar"
                             dismissButtonAriaLabel="Close"
                         >
@@ -59,7 +261,7 @@ const RatingBannerInternal = (props: RatingBannerProps) => {
                                     <div className="ratings">
                                         {ratings.map((e) => (
                                             <label className="rating-number-label">
-                                                <ChoiceGroup
+                                                <StyledChoiceGroup
                                                     className="inlineflex"
                                                     aria-label="Rating"
                                                     label={e}
@@ -83,8 +285,8 @@ const RatingBannerInternal = (props: RatingBannerProps) => {
                                     iconName="CalculatorMultiply"
                                 />
                             </button>
-                        </MessageBar>
-                    </div>
+                        </StyledMessageBar>
+                    </StyledRatingBannerDiv>
                 )}
             {isHatsVisible && (<CS_Hats {...props.hatsProp} />)
             }
