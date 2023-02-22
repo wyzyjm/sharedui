e5df8a2c2efbd972609f83e8ddd23438ba4117d5
Vishak Bharadwaj S <v-vishakb@microsoft.com>
2023-02-22 22:19:05 +0000

    Merged PR 7663264: Added tests for Panel Selector
    # Description
    
    Describe what your change does here.
    
    # Confirm you have done the below:
    
    - [ ] Have you uploaded a screenshot in the description field (if UI has been updated)?
    - [ ] Have you exported the component and type in the index.tsx file?
    - [ ] Have you added tests for the component? Refer to any existing test file for reference.
    - [ ] Have you linked a work item in the PR? If item doesnt exit, create a new work item and link it.
    - [ ] Have you done a self-review? If yes, make sure you sign off on your own PR.
    - [ ] Have you made sure all the accessibility issues have been fixed for the components in the PR?
    - [ ] If adding a new iteration, make sure all the comments are fixed and marked as "resolved" or "wont fix"
    
    ## Reminder
    
    - [ ] If your build fails for your PR, you are needed to fix it.
    
    Added tests for PanelSelector
    
    Related work items: #17282030

---
+++ src/SharedUI.Studios/ClientApp/tests/components/core/ItemList/PanelSelector.spec.tsx
@@ -0,0 +1,53 @@
+import { expect, test, Locator, Page } from '@playwright/test';
+import { StorybookTestPOM } from '../../../StoryBook.spec';
+
+let storybookPOM: StorybookTestPOM;
+
+test.beforeEach(async ({ page }) => {
+    storybookPOM = new StorybookTestPOM(page);
+    await storybookPOM.goto();
+
+    const pageUrl = storybookPOM.getBaseUrl() + "?path=/story/itemlist--item-list";
+    await page.goto(pageUrl);
+});
+
+
+test.describe('Panel Selector tests', () => {
+    test('should render Panel Selector as per design', async ({ page }) => {
+
+        const buttonElement = await storybookPOM.getByRole('button', { name: 'Panel selector' });
+
+        // Check text color
+        const color = await storybookPOM.getComputedStyle(buttonElement, "color");
+        await expect(color).toBe("rgb(50, 49, 48)");
+
+        // Check font size
+        const fontSize = await storybookPOM.getComputedStyle(buttonElement, "font-size");
+        await expect(fontSize).toBe("14px");
+
+        // Check font weight
+        const fontWeight = await storybookPOM.getComputedStyle(buttonElement, "font-weight");
+        await expect(fontWeight).toBe("400");
+
+        // Check background color
+        const headerBackgroundColor = await storybookPOM.getComputedStyle(buttonElement, "background-color");
+        await expect(headerBackgroundColor).toBe("rgb(255, 255, 255)");
+
+    });
+
+    test('the ItemList should update on Panel Selector updates', async ({ page }) => {
+        await (await storybookPOM.getByRole('button', { name: 'Panel selector' })).click();
+
+        //Check ItemList for updates
+        await (await storybookPOM.getLocatorIframe('label:has-text("First name") span')).click();
+        await expect(await storybookPOM.getByRole('button', { name: 'Last name' })).toContainText('Last name');
+    });
+
+    // Accessibility voilations check
+    // test('to verify no accessibility violations are found', async ({ page }) => {
+    //     // Check the Voilations tab under Accessibility
+    //     await page.getByRole('tab', { name: 'Accessibility' }).click();
+    //     await page.locator('button', { hasText: 'Violations' }).click();
+    //     await expect(page.locator('.sto-142f1ph')).toHaveText('0 Violations');
+    // });
+});
